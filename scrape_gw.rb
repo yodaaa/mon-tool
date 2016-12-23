4bf0f091c742f7d14c5d38e71bbb8fac394c6ff6
yodaaa <01ewell0@gmail.com>
2016-12-23 19:56:35 +0900

    csvファイル出力 データ整理をメソッド化

--- scrape_gw.rb
+++ scrape_gw.rb
@@ -18,7 +18,7 @@ mon_data = doc.css('table') #table要素全取得
 puts mon_data
 =end
 
-mon1 =  doc.xpath("//div[@class='monst-table-size']/table[2]/tr")
+mon1 = doc.xpath("//div[@class='monst-table-size']/table[2]/tr")
 mon2 = doc.xpath("//div[@class='js-accordion-area accordion-area'][1]/table[1]/tr")
 
 =begin
@@ -34,35 +34,36 @@ end
 File.open("data/output2.txt", "w") do |f|
   f.puts("#{mon2}")
 end
-
-=begin
-mon1.each do |node|
-  next if node.css('td[1]').empty?
-  p node.css('td[1]').inner_text;
-  next if node.css('td[2]').empty?
-  p node.css('td[2]').inner_text;
-  next if node.css('td[3]/a').empty?
-  p node.css('td[3]/a').attribute('href').value;
-  puts ""
+File.open("data/output_s.csv", "w") do |f|
+  f.puts("")
 end
-=end
-str =""
-mon2.each do |node|
-  if(node.css('td').empty?)
-    str << node.css('th[1]').inner_text
-    str << "\n"
-  else
-    next if node.css('td[1]').empty?
-    str << node.css('td[1]').inner_text
-    next if node.css('td[2]').empty?
-    str << node.css('td[2]').inner_text
-    next if node.css('td[3]/a').empty?
-    str << node.css('td[3]/a').attribute('href').value
-    str << "\n"
+
+def extraSchedule(mon)
+  date_str=""
+  str =""
+  mon.each do |node|
+    if(node.css('td').empty?)
+      date_str = node.css('th[1]').inner_text
+    else
+      str << date_str + ","
+      next if node.css('td[1]').empty?
+      str << node.css('td[1]').inner_text
+      str << ","
+      next if node.css('td[2]').empty?
+      str << node.css('td[2]').inner_text
+      str << ","
+      next if node.css('td[3]/a').empty?
+      str << node.css('td[3]/a').attribute('href').value
+      str << "\n"
+    end
   end
   puts str
+  File.open("data/output_s.csv", "a") do |f|
+    f.puts("#{str}")
+  end
 end
 
-File.open("data/output_s.txt", "w") do |f|
-  f.puts("#{str}")
-end
+extraSchedule(mon1)
+extraSchedule(mon2)
+
+
