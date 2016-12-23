a65f005bde40e34412c894f68ea3fa9181aec446
yodaaa <01ewell0@gmail.com>
2016-12-23 16:48:48 +0900

    イベントスケジュールも取得できるようにした

--- scrape_gw.rb
+++ scrape_gw.rb
@@ -13,19 +13,25 @@ doc = Nokogiri::HTML.parse(html, nil, charset)
 
 =begin
 p doc.title #タイトル取得
-p doc
-mon_data1 = doc.xpath('/html/body/page-wrap/wrapper/main_wrap/main-col-wrap/content-wrap/article-wrap/article-body')
-mon_data2 = doc.css('table')
-puts mon_data2
+p doc       #要素取得
+mon_data = doc.css('table')
+puts mon_data
 =end
 
 mon1 =  doc.xpath("//div[@class = 'monst-table-size']/table[2]")
 mon2 = doc.xpath("//div[@class = 'js-accordion-area accordion-area'][1]/table[1]")
-File.open("output1.txt", "w") do |f|
+
+=begin
+#イベントスケジュール(星4)
+mon3 =doc.xpath("//*[@id='article-body']/table")
+mon4 = doc.xpath("//div[@class = 'js-accordion-area accordion-area'][2]/table[1]")
+=end
+
+File.open("data/output1.txt", "w") do |f|
   f.puts("#{mon1}")
 end
 
-File.open("output2.txt", "w") do |f|
+File.open("data/output2.txt", "w") do |f|
   f.puts("#{mon2}")
 end
-#//*[@id="article-body"]/div[6]
+
